7e00e464ab5e7c9474faeeabeff4effa931d6d6d
smallhenry <henrypotter1008@gmail.com>
2017-09-18 17:19:16 +0800

    第二組調整樣式及查詢功能

--- tp/src/app/mccodelist/mccodelist.component.ts
+++ tp/src/app/mccodelist/mccodelist.component.ts
@@ -9,30 +9,55 @@ import {Router, ActivatedRoute} from '@angular/router';
 })
 export class MccodelistComponent implements OnInit {
 
-  datas;
-  // code: String  = 'FPC';
-  code: String ;
-  date: String  = '20170918';
+  today: Date ;
+  strYear: string = '';
+  strMonth: string = '';
+  strDay: string = '';
+  datas: any[] = [];
+  code: String  ;
+  date: String  ;
 
-  constructor(public datasvc: DatasService,private router:Router,private route:ActivatedRoute) { }
+
+  constructor(public datasvc: DatasService, private router: Router, private route: ActivatedRoute ) {
+    this.today = new Date();
+    this.strYear = this.today.getFullYear().toString();
+
+    this.strMonth = '';
+    let tmpMonth = this.today.getMonth() + 1 ;
+
+    if (tmpMonth.toString().length > 1) {
+      this.strMonth = tmpMonth.toString();
+    }else {
+      this.strMonth = '0'.concat(tmpMonth.toString());
+    }
+
+    this.strDay = this.today.getDate().toString();
+    this.date =  this.strYear.concat('-', this.strMonth, '-' , this.strDay);
+
+
+  }
 
 
   ngOnInit() {
-     this.route.params.subscribe(params=>{
-         this.code=params['code'];
-     });
-     this.doquery();
+    this.route.params.subscribe(params => {
+      this.code = params['code'];
+    });
+  this.doquery();
+
   }
 
 
   doquery() {
-
-
-    this.datasvc.getAssmbingDetail( this.code , this.date)
+    this.datas = [];
+    this.datasvc.getAssmbingDetail( this.code , this.date.replace('-', '').replace('-', ''))
     .subscribe(data => {
+
         this.datas = data;
     });
+
+
   }
+
 
 
 
