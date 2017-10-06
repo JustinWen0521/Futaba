b4e46184a803e76528dfa4b2ebeac0f99a660787
chris <c59430@gmail.com>
2017-10-06 17:36:59 +0800

    Add login

--- tp/src/app/home/home.component.ts
+++ tp/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DatasService } from '../datas.service';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import { IntervalObservable } from 'rxjs/Observable/IntervalObservable';
 
 @Component({
@@ -11,70 +11,143 @@ import { IntervalObservable } from 'rxjs/Observable/IntervalObservable';
 
 export class HomeComponent implements OnInit {
 
-    datas: any;
-    testwidth: any;
-     // futaba圖片放置路徑
-     futaba = {
-      name:  'Futaba',
-      picture: 'assets/icons/futaba.jpg'
-     };
-     private display: boolean; // whether to display info in the component
-     // use *ngIf="display" in your html to take
-     // advantage of this
+  user: string;
+  pws: string;
+  islogin: boolean = false;
 
-     private alive: boolean; // used to unsubscribe from the IntervalObservable
+  datas: any;
+  testwidth: any;
+  // futaba圖片放置路徑
+  futaba = {
+    name: 'Futaba',
+    picture: 'assets/icons/futaba.jpg'
+  };
+  private display: boolean; // whether to display info in the component
+  // use *ngIf="display" in your html to take
+  // advantage of this
 
-     isCollapsed: boolean = true;
+  private alive: boolean; // used to unsubscribe from the IntervalObservable
 
-     toggleCollapse(): void {
-       this.isCollapsed = !this.isCollapsed;
-     }
+  isCollapsed: boolean = true;
+
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 
   onResize(event) {
-      this.testwidth = event.target.innerWidth;
-   }
+    this.testwidth = event.target.innerWidth;
+  }
 
-   getCSSClasses(flag: string) {
+  getCSSClasses(flag: string) {
     let cssClasses;
     if (flag === '1024') {
-         cssClasses = {
-      'productTitle1024': true
-   };
+      cssClasses = {
+        'productTitle1024': true
+      };
     } else {
-         cssClasses = {
-      'two': true,
-      'four': false
-   };
+      cssClasses = {
+        'two': true,
+        'four': false
+      };
     }
     return cssClasses;
- }
+  }
 
-   // tslint:disable-next-line:one-line
-   constructor(private dataSvc: DatasService){
+  // tslint:disable-next-line:one-line
+  constructor(private dataSvc: DatasService) {
     this.display = false;
     this.alive = true;
-   }
+  }
 
-   ngOnInit() {
+  ngOnInit() {
     //////////
-    this.dataSvc.getAllDatas()
-    .subscribe( data => {
-       this.datas = data;
-       this.display = true;
-    });
-
-    // get our data every subsequent 60 seconds
-    IntervalObservable.create(60000)
-    .subscribe(() => {
-      this.dataSvc.getAllDatas()
-        .subscribe(data => {
-          this.datas = data;
-        });
-    });
+    console.log( "ngOnInit" +  document.cookie);
+    if (document.cookie == "Y") {
+      this.islogin = true;
+      this.showData();
+      //this.doquery();
+      document.getElementById("btn_link").style.display = "block";
+      document.getElementById("btn_login").style.display = "none";
+      document.getElementById("btn_logout").style.display = "block";
+    }
+    else {
+      //login();
+      console.log('TODO login modal');
+      document.getElementById("btn_login").style.display = "block";
+      document.getElementById("btn_logout").style.display = "none";
+    }
 
   }
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnDestroy() {
     this.alive = false; // switches your IntervalObservable off
   }
+
+  doquery() {
+    console.log( "doquery" +  this.islogin);
+    if (this.islogin) {
+      this.logout();
+    }
+    else {
+      this.login();
+    }
+  }
+
+  logout() {
+    document.cookie = '';
+    this.islogin = false;
+    document.getElementById("btn_link").style.display = "none";
+    this.datas = null;
+
+    document.getElementById("btn_login").style.display = "block";
+    document.getElementById("btn_logout").style.display = "none";
+
+  }
+
+  login() {
+    // if (this.islogin)
+    //   return;
+    console.log(this.pws);
+
+    if (this.user.toLowerCase() != "tftp")
+    {
+      alert("帳密錯誤");
+      return;
+    }
+    if (this.pws != "76025017") {
+      alert("帳密錯誤");
+      return;
+    }
+    document.cookie = "Y; path=/";
+    //document.getElementById("btn_login").style.display="none";
+    document.getElementById("btn_link").style.display = "block";
+    document.getElementById("btn_login").style.display = "none";
+    document.getElementById("btn_logout").style.display = "block";
+    this.showData();
+
+
+
+  }
+
+  showData() {
+    this.dataSvc.getAllDatas()
+    .subscribe(data => {
+      this.datas = data;
+      this.display = true;
+    });
+
+  // get our data every subsequent 60 seconds
+  IntervalObservable.create(60000)
+    .subscribe(() => {
+      if (this.islogin)
+      {
+        this.dataSvc.getAllDatas()
+        .subscribe(data => {
+          this.datas = data;
+        });
+      }
+    });
+
+  }
+
 }
